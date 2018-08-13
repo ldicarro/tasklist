8c6dfb1e1b006019fe34c767f3be2c391f47e698
Louis <louis@labelinteractive.com>
2018-08-13 12:41:49 -0400

    -fix OPTIONS under auth

--- noderest-api/api/controllers/authenticationController.js
+++ noderest-api/api/controllers/authenticationController.js
@@ -2,17 +2,21 @@
 
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
-const Task = mongoose.model('Tasks');
 const config = require('../../config');
 
 
 exports.check_token = (req, res, next) => {
 
-	if(req.method === 'OPTIONS') { next(); }
-	const appkey = req.headers['x-application-key'];
+	if(req.method === 'OPTIONS') { 
+		next(); 
+	}
+	else
+	{
+		const appkey = req.headers['x-application-key'];
 
-	if (!appkey) { return res.status(401).json({ message: 'Unauthorized' }); }
-	if (appkey !== config.appkey) { return res.status(401).json({ message: 'Authorization failed' }); }
-	if (appkey === config.appkey) { next(); }
+		if (!appkey) { return res.status(401).json({ message: 'Unauthorized' }); }
+		if (appkey !== config.appkey) { return res.status(401).json({ message: 'Authorization failed' }); }
+		next();
+	}
 
 };
